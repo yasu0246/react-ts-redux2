1782e94250ecfd2640441391f85dd9d3a64d5b1a
yasu0246 <miuy0246@gmail.com>
2022-08-19 18:48:16 +0900

    remove

--- src/features/todos/todosSlice.ts
+++ src/features/todos/todosSlice.ts
@@ -1,7 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
-import type { TodoInput, Todo } from './types';
-import { createTodo } from './crud/create';
+import type { TodoInput, Todo, TodoId } from './types';
+import { createTodo, removeTodo } from './crud';
+import { RootState} from '../../app/store'
+
 
 export type TodoState = {
   todos: Todo[];
@@ -23,9 +25,21 @@ export const todosSlice = createSlice({
       const todo = createTodo(action.payload);
       state.todos.push(todo);
     },
+
+    remove: (state, action: PayloadAction<TodoId>) => {
+      const id = action.payload;
+      const index = state.todos.findIndex((todo) => todo.id === id);
+      const todo = state.todos[index];
+      if (!todo) return;
+
+      state.todos[index] = removeTodo(todo);
+    },
   },
 });
 
-export const { create } = todosSlice.actions;
+export const { create,remove } = todosSlice.actions;
+
+export const selectTodos = (state: RootState) =>
+  state.todos.todos.filter((todo) => todo.deletedAt === undefined);
 
 export default todosSlice.reducer;
